fec34f9a9b1a404d9d238115b977f3f03d9cb490
Altair1618 <farhannabilsuryono3@gmail.com>
2023-11-27 14:23:52 +0700

    chore: add payload types for ticket

--- Ticket_Service/app/common/structs/ticket.go
+++ Ticket_Service/app/common/structs/ticket.go
@@ -1,5 +1,28 @@
 package commonStructs
 
+import "github.com/google/uuid"
+
+type TicketStatus string
+
+const (
+	Open    TicketStatus = "OPEN"
+	Ongoing TicketStatus = "ONGOING"
+	Booked  TicketStatus = "BOOKED"
+)
+
+type CreateTicketServicePayload struct {
+	Price   int       `json:"price" form:"price" validate:"required"`
+	EventId uuid.UUID `json:"eventId" form:"eventId" validate:"required"`
+	SeatId  string    `json:"seatId" form:"seatId" validate:"required"`
+}
+
+type UpdateTicketServicePayload struct {
+	Price   int          `json:"price" form:"price"`
+	EventId uuid.UUID    `json:"eventId" form:"eventId"`
+	SeatId  string       `json:"seatId" form:"seatId"`
+	Status  TicketStatus `json:"status" form:"status"`
+}
+
 type UpdateStatusServicePayload struct {
 	InvoiceId string        `json:"invoiceId" form:"invoiceId"`
 	Status    PaymentStatus `json:"status" form:"status"`
