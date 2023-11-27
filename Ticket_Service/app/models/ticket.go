d57e78bfd6cafc3ce3ec93b25f9e82d0a614aeec
Altair1618 <farhannabilsuryono3@gmail.com>
2023-11-28 05:56:18 +0700

    fix: seat unique constraint

--- Ticket_Service/app/models/ticket.go
+++ Ticket_Service/app/models/ticket.go
@@ -10,8 +10,8 @@ import (
 type Ticket struct {
 	Id        uuid.UUID                  `gorm:"type:uuid;default:gen_random_uuid();primaryKey;column:id"`
 	Price     int                        `gorm:"not null;column:price"`
-	EventId   uuid.UUID                  `gorm:"type:uuid;not null;column:event_id"`
-	SeatId    string                     `gorm:"not null;column:seat_id;unique"`
+	EventId   uuid.UUID                  `gorm:"type:uuid;not null;column:event_id;uniqueIndex:unique_seat_constraint"`
+	SeatId    string                     `gorm:"not null;column:seat_id;uniqueIndex:unique_seat_constraint"`
 	Status    commonStructs.TicketStatus `gorm:"not null;default:OPEN;column:status"`
 	CreatedAt time.Time                  `gorm:"autoCreateTime;column:created_at"`
 	UpdatedAt time.Time                  `gorm:"autoUpdateTime;column:updated_at"`
