aeaf1fb1f252748334086c791ad00f1a0ac4fb5b
Enliven26 <16521443@mahasiswa.itb.ac.id>
2023-11-27 11:08:37 +0700

    feat: history page

---
+++ Client_App/src/lib/components/TableContent.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    export let isHeader: Boolean = false
+</script>
+
+{#if isHeader}
+    <th class="font-normal text-sm p-2"><slot/></th>
+{:else}
+    <td class="px-2 py-[20px]"><slot/></td>
+{/if}
