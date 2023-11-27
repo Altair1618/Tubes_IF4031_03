09b2d1f2d089c2a714bb05390dcc20ea52fab304
Altair1618 <farhannabilsuryono3@gmail.com>
2023-11-27 10:20:14 +0700

    refactor: change module name

--- Ticket_Service/app/controllers/ticket/updateStatus.go
+++ Ticket_Service/app/controllers/ticket/updateStatus.go
@@ -1,8 +1,8 @@
 package ticketController
 
 import (
-	ticketService "github.com/Altair1618/IF4031_03_Ticket/app/services/ticket"
-	"github.com/Altair1618/IF4031_03_Ticket/app/utils"
+	ticketService "github.com/Altair1618/Tubes_IF4031_03/Ticket_Service/app/services/ticket"
+	"github.com/Altair1618/Tubes_IF4031_03/Ticket_Service/app/utils"
 	"github.com/gofiber/fiber/v2"
 )
 
