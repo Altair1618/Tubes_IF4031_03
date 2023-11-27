a021363cb68ee9d7a4e08da395dd4a26287fbfde
Altair1618 <farhannabilsuryono3@gmail.com>
2023-11-27 10:31:10 +0700

    chore: struct name json

--- Ticket_Service/app/models/ticket.go
+++ Ticket_Service/app/models/ticket.go
@@ -15,10 +15,10 @@ const (
 )
 
 type Ticket struct {
-	Id        uuid.UUID    `gorm:"type:uuid;default:gen_random_uuid();primaryKey"`
-	EventId   uuid.UUID    `gorm:"type:uuid;not null"`
-	SeatId    string       `gorm:"not null"`
-	Status    TicketStatus `gorm:"not null;default:OPEN"`
-	CreatedAt time.Time    `gorm:"autoCreateTime"`
-	UpdatedAt time.Time    `gorm:"autoUpdateTime"`
+	Id        uuid.UUID    `gorm:"type:uuid;default:gen_random_uuid();primaryKey" json:"id"`
+	EventId   uuid.UUID    `gorm:"type:uuid;not null" json:"event_id"`
+	SeatId    string       `gorm:"not null" json:"seat_id"`
+	Status    TicketStatus `gorm:"not null;default:OPEN" json:"status"`
+	CreatedAt time.Time    `gorm:"autoCreateTime" json:"created_at"`
+	UpdatedAt time.Time    `gorm:"autoUpdateTime" json:"updated_at"`
 }
