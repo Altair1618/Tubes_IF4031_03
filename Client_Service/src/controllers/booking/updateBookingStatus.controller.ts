d269096ac3b7ab69eb33b5f60f4297542aa6a905
sozyGithub <thinpotatogithub@gmail.com>
2023-11-29 15:20:50 +0700

    temp

---
+++ Client_Service/src/controllers/booking/updateBookingStatus.controller.ts
@@ -0,0 +1,14 @@
+import Elysia from "elysia";
+import parseJWTMiddleware from "../../middlewares/parseJWTMiddleware";
+import authMiddleware from "../../middlewares/authMiddleware";
+
+const updateBookingStatusController = new Elysia().use(parseJWTMiddleware).get(
+  "/:id", 
+  async () => {
+
+  },{ beforeHandle: [({ auth:{ success, message}}) => {
+    return authMiddleware(success, message)
+  }]}
+)
+
+export default updateBookingStatusController
--- Client_Service/src/models/booking.ts
+++ Client_Service/src/models/booking.ts
@@ -1,25 +1,27 @@
 import { pgTable, timestamp, varchar, uuid, pgEnum } from "drizzle-orm/pg-core";
-import { user } from "./user"; 
+import { user } from "./user";
 
 export enum BookingStatus {
 	SUCCESS = "SUCCESS",
-	IN_QUEUE = "IN QUEUE", 
-	WAITING_FOR_PAYMENT = "WAITING FOR PAYMENT", 
-	PURCHASING = "PURCHASING", 
-	FAILED = "FAILED"
+	IN_QUEUE = "IN QUEUE",
+	WAITING_FOR_PAYMENT = "WAITING FOR PAYMENT",
+	PURCHASING = "PURCHASING",
+	FAILED = "FAILED",
 }
 
 export const bookingStatus = pgEnum(
-	"booking_status", 
-	Object.values(BookingStatus) as [string]
-)
+	"booking_status",
+	Object.values(BookingStatus) as [string],
+);
 
 export const booking = pgTable("booking_history", {
 	id: uuid("id").primaryKey().defaultRandom(),
 	ticketId: uuid("ticket_id").unique().notNull(),
 	status: bookingStatus("status").notNull(),
-    report: varchar("report"),
-    userId: varchar("user_id", {length: 15}).notNull().references(() => user.id),
+	report: varchar("report"),
+	userId: varchar("user_id", { length: 15 })
+		.notNull()
+		.references(() => user.id),
 	createdAt: timestamp("created_at", { mode: "string" }).notNull().defaultNow(),
 	updatedAt: timestamp("updated_at", { mode: "string" }).notNull().defaultNow(),
 });
