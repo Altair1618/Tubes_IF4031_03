9d266f65e628152efefb096b5c6967ddd86defc1
Altair1618 <farhannabilsuryono3@gmail.com>
2023-11-29 14:28:38 +0700

    fix: add available seats in event detail

--- Ticket_Service/app/services/event/getEventById.go
+++ Ticket_Service/app/services/event/getEventById.go
@@ -3,6 +3,7 @@ package eventService
 import (
 	"fmt"
 
+	commonStructs "github.com/Altair1618/Tubes_IF4031_03/Ticket_Service/app/common/structs"
 	"github.com/Altair1618/Tubes_IF4031_03/Ticket_Service/app/configs"
 	"github.com/Altair1618/Tubes_IF4031_03/Ticket_Service/app/models"
 	"github.com/Altair1618/Tubes_IF4031_03/Ticket_Service/app/utils"
@@ -12,6 +13,7 @@ import (
 
 func GetEventByIdService(id uuid.UUID) utils.ResponseBody {
 	db, _ := configs.GetGormClient()
+	eventResponse := new(commonStructs.EventDetailResponse)
 
 	var event models.Event
 	result := db.First(&event, "id = ?", id)
@@ -24,19 +26,30 @@ func GetEventByIdService(id uuid.UUID) utils.ResponseBody {
 			Message: "Error While Fetching Data From Database",
 			Data:    nil,
 		}
-	} else {
-		if result.RowsAffected == 0 {
-			return utils.ResponseBody{
-				Code:    404,
-				Message: "Event Not Found",
-				Data:    nil,
-			}
-		}
-		
+	}
+
+	if result.RowsAffected == 0 {
 		return utils.ResponseBody{
-			Code:    200,
-			Message: "Event Data Fetched Successfully",
-			Data:    fiber.Map{"event": event},
+			Code:    404,
+			Message: "Event Not Found",
+			Data:    nil,
 		}
+	}
+
+	var availableSeats int64
+	db.Model(&models.Ticket{}).Where("event_id = ? AND status = ?", id, commonStructs.Open).Count(&availableSeats)
+	
+	eventResponse.Id = event.Id
+	eventResponse.EventName = event.EventName
+	eventResponse.EventTime = event.EventTime
+	eventResponse.Location = event.Location
+	eventResponse.CreatedAt = event.CreatedAt
+	eventResponse.UpdatedAt = event.UpdatedAt
+	eventResponse.AvailableSeats = int(availableSeats)
+
+	return utils.ResponseBody{
+		Code:    200,
+		Message: "Event Data Fetched Successfully",
+		Data:    fiber.Map{"event": eventResponse},
 	}
 }
